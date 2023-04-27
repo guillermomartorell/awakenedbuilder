7947638e425aa62ff9c621d7af65cca4067d0139
Guillermo Martorell <guillermo.martorell@gmail.com>
2023-04-27 07:21:48 -0300

    refactor sections

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Attribute {
+  label: string;
+  value: number;
+  type: string;
+}
+
 @Component({
   selector: 'app-section',
-  template: ` <div class="wrapper">
-      <h4>{{ desc }}</h4>
+  template: `
+    <div class="wrapper">
+      <h4>{{ description }}</h4>
     </div>
     <div class="wrapper">
       <div class="attr-section" *ngFor="let attributeGroup of data">
-        <h4>
-          {{ attributeGroup[0].type }}: {{ attrSum[attributeGroup[0].type] }}
-        </h4>
+        <h4>{{ attributeGroup[0].type }}: {{ attrSum[attributeGroup[0].type] }}</h4>
         <div class="attr" *ngFor="let attribute of attributeGroup">
           <strong>{{ attribute.label }}</strong>
           <div>
@@ -20,16 +25,16 @@ import { Component, Input, OnInit } from '@angular/core';
           </div>
         </div>
       </div>
-    </div>`,
+    </div>
+  `,
   styleUrls: ['./section.component.css'],
 })
 export class SectionComponent implements OnInit {
-  @Input() data: any[] = [];
-  @Input() type: any;
-
+  @Input() data: any[][] = [];
+  @Input() type: string = '';
   attrSum: Record<string, number> = {};
   attrObj: any[] = [];
-  desc: string = 'Description: []';
+  description: string = 'Description: []';
 
   ngOnInit() {
     for (const attributeGroup of this.data) {
@@ -40,25 +45,28 @@ export class SectionComponent implements OnInit {
   handleRated(value: number, type: string, label: string) {
     const insert: any = { label, value, type };
     const index = this.attrObj.findIndex((el) => el.label === label);
+
     if (index === -1) {
       this.attrObj.push(insert);
     } else {
       this.attrObj[index] = insert;
     }
 
-    this.attrSum[type] =
-      this.type === 'attributes'
-        ? this.attrObj
-            .filter((item) => item.type === type)
-            .reduce((sum, item) => sum + item.value - 1, 0)
-        : this.attrObj
-            .filter((item) => item.type === type)
-            .reduce((sum, item) => sum + item.value, 0);
+    const sum = this.attrObj
+      .filter((item) => item.type === type)
+      .reduce((sum, item) => {
+        if (this.type === 'attributes') {
+          return sum + item.value - 1;
+        }
+        return sum + item.value;
+      }, 0);
 
-    this.data.forEach((element) => {
-      element.forEach((el: any) => {
-        if (el.label === label) {
-          this.desc = label + ': ' + el.val[value - 1];
+    this.attrSum[type] = sum;
+
+    this.data.forEach((attributeGroup) => {
+      attributeGroup.forEach((attribute) => {
+        if (attribute.label === label) {
+          this.description = `${label}: ${attribute.val[value - 1]}`;
         }
       });
     });
