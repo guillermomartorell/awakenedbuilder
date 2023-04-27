ac16932d14ecd8d8ddba8acd62bd54ec82e5f1f9
guillermomartorell <1244512+guillermomartorell@users.noreply.github.com>
2023-04-27 16:54:44 -0300

    adds description possessed by and specialty

--- src/assets/skills.ts
+++ src/assets/skills.ts
@@ -11,6 +11,9 @@ export const skills = [
         "•••• Expert: You’ve already noted three attackersand mapped four avenues of escape.",
         "••••• Master: Those ninjas might as well be wearing pink neon clown suits.",
       ],
+      description: "You sense things coming long before anyone else does. A combination of keen instincts and sharp thinking, this Ability helps you size up situations and respond accordingly. Beast-folk, survivors, bright courtiers and battle-honed veterans have high Alertness ratings.",
+      possessed: "Spies, Bodyguards, Feral Kids, Street Survivors, Shapeshifters, War Veterans",
+      specialty: "Ambushes, Instincts, Omens, Urban Wastelands, Combat Zones, Covert Pursuit, Warning Signs",
     },
     {
       label: "Art",
@@ -22,6 +25,9 @@ export const skills = [
         "•••• Expert: You’re a working pro whose work displays fascinating appeal and skill.",
         "••••• Master: You’re an acknowledged master of your field.",
       ],
+      description: "Also known as Artistic Expression, this Talent reflects your ability with a visual or performing art: drawing, singing, acting, writing, painting, dance, sculpture, and so forth. Given such talents, you can work within your chosen medium to produce works of startling power. Depending on your idiom, you might create realistic renderings, engaging exaggerations, or wildly avant-garde experimental art. Higher levels reflect your ability to impress your audience; in art, that means striking a chord of Truth in people even when your work isn’t remotely realistic. As with other related general Abilities, each specialty reflects a different medium or field, of artistic endeavor. Each type of art – dance, painting, etc. – must be purchased as a separate specialty. However, because the principles of art are related even when the techniques differ, you can purchase additional specialties for only four points, assuming you have at least four dots in Art and one specialty already in place. See The Well- Skilled Craftsman (p. 279) for further details.",
+      possessed: "Art Students, Fine Artists, Professional Artists, Talented Kids, Dedicated Hobbyists, Reclusive Geniuses, Meditative Ascetics, Mages Who Focus Magick Through Art",
+      specialty: "Painting, Video, Singing, Acting, Dance, Mixed Media, CGI, Model-Making, Classical Art, Avant-Garde, Unrefined Intuition, Mad Brilliance, Uncanny Truths, Primitive Techniques, specific musical instruments (Guitar, Drums, Piano, etc.)",
     },
     {
       label: "Athletics",
@@ -33,6 +39,9 @@ export const skills = [
         "•••• Expert: Gym-rats and pro athletes take notes from you.",
         "••••• Master: You could qualify for the Olympics… and you might actually compete!",
       ],
+      description: "Fitness is your passion. While other folks sit on the sidelines, you’re running races, scoring touchdowns, biking to and from work, and developing prowess with intense practice. Folks don’t usually picture wizards hanging around the gym, but you’re an exception to that stereotype. “A sound mind in a sound body” is your motto. What good is it to be a master of reality, after all, if your reality involves a fat ass and flabby gut? Like Art, Athletics has many different specialties – running, climbing, swimming, and so forth. And like Art, Crafts, and other broad categories, you can buy specialties to reflect different athletic pursuits. (The Well-Skilled Craftsman option applies here as well.)",
+      specialty: "Bodybuilding, Cycling, Climbing, Equestrian, Swimming, Survival Training, Extreme Endurance, Flow- Arts, Parkour, Skiing, specific sports (Football, Basketball, Hockey, etc.)",
+      possessed: "Jocks, Cyborgs, Fitness Gurus, Personal Trainers, Professional Dancers, Martial Artists, Underwear Models, TV Stars",
     },
     {
       label: "Awareness",
@@ -44,6 +53,9 @@ export const skills = [
         "•••• Expert: The supernatural world reveals itself to you in disturbing detail.",
         "••••• Master: You’re so attuned to the mystic realm that many folks consider you insane.",
       ],
+      description: "You’ve got uncanny perceptions. While alert folks spot everyday clues, your instincts cue in on the so-called supernatural side of life. Perhaps you’ve simply got that feeling about things – some people do, even if they’re not Awakened as such. More likely, you’ve spent enough time around the magical world to sense its effects in your presence. At lower levels, this Talent grants a nebulous perception of uncanny phenomena; higher ratings in the Trait reveal auras, expose the secretive Night-Folk, and open your eyes to the spiritual Periphery. In game terms, you can use Perception + Awareness to spot paranormal effects, detect magick, recognize Resonance Echoes, and so forth, assuming they’re within close proximity of you. The more successes you roll, the more detailed your impressions become. For a reference chart of aura colors and textures, see Chapter Ten, (p.507).",
+      possessed: "Canny Grannies, Wise Spies, Crazy Shamans, Demented Seers, Insightful Kids, Mad Scientists, Those Touched By God",
+      specialty: "Omens, Auras, Resonance, Weird Feelings, Mystic Instincts, Hidden Magic, Spiritual Vidare",
     },
     {
       label: "Brawl",
@@ -55,6 +67,9 @@ export const skills = [
         "•••• Expert: Extreme fighting champs shiver when you enter the ring.",
         "••••• Master: “See me hit you, you go down.”",
       ],
+      description: "You kick ass. Maybe you grew up hard and mean, or maybe you’re schooled in basic self-defense. Either way, your fists and feet are weapons – probably not deadly, but a lot more effective than random swings from unskilled brutes. Not only do you know how to fight, but you’re naturally good at it. You’ve got the skill, will, and training to hurt someone using nothing more than human cunning and animal ferocity. (For armed fighting, see the Melee Skill, and for refined unarmed techniques, see the Martial Arts Skill. Both appear below.)",
+      possessed: "Bikers, Brawlers, Bullies, Former Punching Bags, Bodyguards, Military Veterans, Scary Folks With Bad Tempers",
+      specialty: "Barroom, Boxing, Beast-Form Fighting, Dirty Fighting, Disarming, Peaceful Warrior",
     },
     {
       label: "Empathy",
@@ -66,6 +81,9 @@ export const skills = [
         "•••• Expert: Even below the surface, a person’s true feelings seem obvious to you.",
         "••••• Master: To your eyes, most people are open books.",
       ],
+      description: "An innate sense of emotional energies, this Talent provides you with insight – perhaps more insight than you might want – into how the people around you feel. Ideally, this helps you understand them better and perhaps allows you to calm, rouse, or otherwise adjust their emotional state. On the flipside, it might leave you vulnerable to their passions (a common fault among Ecstatic mages), or it could leave them vulnerable to your machinations. Combined with Mindbased Effects, Empathy can be a potent tool – or weapon – in the hands of an Awakened character. For details, see Mundane Skills and Magickal Effects in Chapter Ten, (pp. 532-535).",
+      possessed: "Ecstatic Mages, Shamans, Healers, Seducers, Sales Reps, Therapists, Trusted Friends, Nephandi",
+      specialty: "Hidden Feelings, Subtle Cues, Emotional Manipulation, Trust, Sensing Emotions, Life-Force Fluctuations, Interpersonal Psychology, Seeing Past the Mask, “I Know What You Need”",
     },
     {
       label: "Expression",
@@ -77,6 +95,9 @@ export const skills = [
         "•••• Expert: In this media-glutted world, you still blaze bright.",
         "••••• Master: Poets, prophets, and politicians wish they were you. Possessed By: Rock Stars,",
       ],
+      description: "Life is your canvas, and you excel at making impressions upon it. Through theatrical flair and audience psychology, you can rouse emotions, shift opinions, and grab the spotlight in social situations. Although this Trait can be used to handle any social situation, it’s best used to make general impressions. For specific artistic endeavors, see Art, above.",
+      possessed: "Rock Stars, Media Pundits, Ecstatic Mages, Executives, Poets, Leaders, Politicians, Bloggers, Rabble- Rousers, Hip-Hop MCs, Professional Celebrities",
+      specialty: "Passion Plays, Poetic Eloquence, Motivational Speaking, Tactical Rhetoric, Preaching, Command, Shut-Downs, Rocking the Mic",
     },
     {
       label: "Intimidation",
@@ -88,6 +109,9 @@ export const skills = [
         "•••• Expert: Alpha wolf.",
         "••••• Master: Queen or king of all you survey.",
       ],
+      description: "You scare people – perhaps not through overt threats (although, of course, that works too), but by the simple virtue of your presence. When you choose to, you can dominate most situations. Maybe you understand the unconscious cues of social alpha-ism or have a way with cutting words. Or perhaps you’re simply big enough, mean enough or don’t-give-a-fuck enough to inspire submission. Whatever your secret might be, this Talent gives you an edge when staring down other people, beasts, and possibly other entities as well.",
+      possessed: "Men in Black, Cyborgs, Cops, Witches, Mad Scientists, Outlaws, Bullies, Beast-Folk, Pro Doms, Drill Sergeants, Samuel L. Jackson",
+      specialty: "Bad Cop, BDSM, Pack Hierarchy, Cold Stare, Brutal Threats, Topping From Below, Pants- Crapping Terror"
     },
     {
       label: "Leadership",
@@ -99,6 +123,9 @@ export const skills = [
         "•••• Expert: Tycoon.",
         "••••• Master: Nick Fury.",
       ],
+      description: "A more refined sort of control, this Talent helps you inspire other folks to follow your lead. It might involve commanding presence, assertive words, clear determination, or simply the Right Stuff when said stuff is needed. When you bark, folks hop – not always with compliance, but with, at the very least, a grudging admission that you seem to know what you’re doing… even when you don’t.",
+      possessed: "Officers, Managers, Pundits, Preachers, Teachers, Team Leaders, Cult Leaders, Gang Leaders, Politicians",
+      specialty: "Competent, Inspiring, Regal, Harsh, Do-Or-Die, Soft-Voiced Command",
     },
     {
       label: "Streetwise",
@@ -110,6 +137,9 @@ export const skills = [
         "•••• Expert: Gang leader.",
         "••••• Master: Gang lord. Possessed By: Bartenders, Hookers,",
       ],
+      description: "The underworld is your home, and you know it pretty well. Folks who normally don’t talk to no one will open up to you. Probably raised in or around the streets, you understand the codes and cues outside the law. In addition to some minor tricks of the trade (jimmying locks, fencing goods, swiping stuff when no one’s looking), you know how to move and speak and otherwise fit in where cops and angels fear to tread.",
+      possessed: "Bartenders, Hookers, Hackers, Pimps, Street People, Cops, Detectives, Street Ministers, Musicians, Runaways, Drug Dealers, Activists, Journalists, Taxi Drivers",
+      specialty: "Theft, Survival Skills, Outlaw Hacker Culture, Crash Space, Street Politics, Urban Tribes, Trust, Hiding Spots, Sex Trade, Drug Trade, Music Culture, Gangs, Seeing Nothing Yet Everything",
     },
     {
       label: "Subterfuge",
@@ -121,6 +151,9 @@ export const skills = [
         "•••• Expert: Career politician.",
         "••••• Master: Master spy.",
       ],
+      description: "You’re good at winning people’s trust and then turning it to your advantage – and also good at spotting folks who try to do the same thing to you. (This second element, incidentally, replaces the old Ability Sense Deception.) Perhaps you use your powers for good… but probably not. A trade secret for sneaks, gossips, grifters, and frauds, this Talent helps you get around social barriers, figure out what people want, and then appeal to their desires. It doesn’t have to involve outright lying, but let’s just say you’re really good at lawyering the truth.",
+      possessed: "Syndicate Agents, Secret Agents, Home- Wreckers, Ad Agency Personnel, Lobbyists, Crooks, Shapeshifters, Infiltrators, Embezzlers, Hustlers, Attorneys, Sociopaths",
+      specialty: "Con Games, Sexual Manipulation, False Innocence, Politics, Law, Cheating, Undermining Opposition, Sensing Deceptions",
     }
   ],
   [
